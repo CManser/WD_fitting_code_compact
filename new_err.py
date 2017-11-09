98aef8acdcd653e6e31204b1a33ead6c354c9482
Christopher Manser <c.j.manser92@googlemail.com>
2017-11-09 17:10:07 +0000

    speeding

--- new_err.py
+++ new_err.py
@@ -14,10 +14,10 @@ def tmp_func(_T, _g, _rv, _sn, _l, _m):
         m_wave_n, m_flux_n, sn_w =norm_model[:,0]*(_rv+c)/c, norm_model[:,1], _sn[:,0]
         m_flux_n_i = interpolate.interp1d(m_wave_n,m_flux_n,kind='linear')(sn_w)
         #Initialise: normalised models and spectra in line region, and chi2
-        tmp_lines_m, lines_s, sum_l_chi2 = [], [], 0
+        tmp_lines_m, lines_s, sum_l_chi2 = [],[],0
         for i in range(len(_l)):
             #crop model and spec to line iff line region is entirely covered by spec
-            l_c0, l_c1 = _l[i,0], _l[i,1]
+            l_c0,l_c1 = _l[i,0],_l[i,1]
             if (l_c1 < sn_w.max()) & (l_c0 > sn_w.min()):
                 l_m = m_flux_n_i.transpose()[(sn_w>=l_c0)&(sn_w<=l_c1)].transpose()
                 l_s = _sn[(sn_w>=l_c0)&(sn_w<=l_c1)]
